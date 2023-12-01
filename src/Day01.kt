0c421a7409c5624a4d5ca33dd3bb2edd9374308b
Zack Mayoh <zach.mayoh@calian.com>
2023-12-01 17:00:52 -0600

    Add part 1 solution and clean up

--- src/Day01.kt
+++ src/Day01.kt
@@ -13,44 +13,49 @@ fun main() {
         "nine" to 9
     )
 
-    fun getFirstDigit(line: String, isReversed: Boolean): Int {
-        line.indices.forEach { i ->
-            when {
-                line[i].isDigit() -> return line[i].digitToInt()
-            }
-            (3 until 6).forEach { j ->
+    fun part1(input: List<String>): Int {
+        fun getFirstDigit(line: String) = line.first(Char::isDigit).digitToInt()
+
+        fun getCalibrationValue(line: String): Int {
+            return getFirstDigit(line.reversed()) + getFirstDigit(line) * 10
+        }
+
+        return input.sumOf(::getCalibrationValue)
+    }
+
+    fun part2(input: List<String>): Int {
+        fun getFirstDigit(line: String, isReversed: Boolean): Int {
+            line.indices.forEach { i ->
                 when {
-                    i + j <= line.length -> {
-                        var key = line.subSequence(i, i + j)
-                        when {
-                            isReversed -> key = key.reversed().toString()
-                        }
-                        when {
-                            numberMap.containsKey(key) -> return numberMap[key]!!
+                    line[i].isDigit() -> return line[i].digitToInt()
+                }
+                (3 until 6).forEach { j ->
+                    when {
+                        i + j <= line.length -> {
+                            var key = (line.subSequence(i, i + j))
+                            key = if (isReversed) key.reversed().toString() else key
+                            when {
+                                numberMap.containsKey(key) -> return numberMap[key]!!
+                            }
                         }
                     }
                 }
             }
+            return -1
         }
-        return -1
-    }
 
-    fun getCalibrationValue(line: String): Int {
-        return getFirstDigit(line.reversed(), true) + getFirstDigit(line, false) * 10
-    }
+        fun getCalibrationValue(line: String): Int {
+            return getFirstDigit(line.reversed(), true) + getFirstDigit(line, false) * 10
+        }
 
-    // 23krgjlpone
-    fun part1(input: List<String>): Int {
-        return input.sumOf { getCalibrationValue(it) }
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf(::getCalibrationValue)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 281)
+    val testInput1 = readInput("Day01_test1")
+    check(part1(testInput1) == 142)
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
